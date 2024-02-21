b707c1896e48ecdedc051cf49ae0797214e57f49
Adamiasty <eskarbnica@gmail.com>
2024-02-21 13:42:43 +0100

    zadanie pop dzien 4

--- Projekt21/Projekt21/Program.cs
+++ Projekt21/Projekt21/Program.cs
@@ -3,14 +3,14 @@ var sex = "kobieta";
 var age = 30;
 
 if (sex == "kobieta" && age < 30) 
-    {
-        Console.WriteLine("Kobieta poniżej 30 lat");
-    }
+{
+Console.WriteLine("Kobieta poniżej 30 lat");
+}
 else if (name == "Ewa" && age == 30 && sex == "kobieta")
-    {
-        Console.WriteLine("Ewa, lat 30");
-    }
+{
+Console.WriteLine("Ewa, lat 30");
+}
 else if (sex != "kobieta" && age < 18)
-    {
-    Console.WriteLine("Niepełnoletni mężczyzna");
-    }
+{
+Console.WriteLine("Niepełnoletni mężczyzna");
+}
