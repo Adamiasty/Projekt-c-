804b6205725f7883c6a9257af4b5a87b35108b5f
Adamiasty <eskarbnica@gmail.com>
2024-02-21 13:07:55 +0100

    Zadanie domowe dzień 4

--- Projekt21/Projekt21/Program.cs
+++ Projekt21/Projekt21/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+var name = "Ewa";
+var sex = "kobieta";
+var age = 30;
+
+if (sex == "kobieta" && age < 30) 
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+else if (name == "Ewa" && age == 30 && sex == "kobieta")
+    {
+        Console.WriteLine("Ewa, lat 30");
+    }
+else if (sex != "kobieta" && age < 30)
+    {
+    Console.WriteLine("Niepełnoletni mężczyzna");
+    }
